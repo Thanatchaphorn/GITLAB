aec241d40f878803e81c291d70bbb8ea531cb982
Thanatchaphorn <flukegazaa@gmail.com>
2015-04-24 11:01:59 +0700

    Modified TextLength.c

--- TextLength.c
+++ TextLength.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-// อย่าลืม include <string.h> ด้วยนะครับ
 #include <string.h>
 int main()
 {
-    char s[100];
+    char s[150];
  
-    printf("Input string : ");
+    printf("Input String : ");
     gets(s);
  
     printf("\n\nString ( %s ) length is ( %d ) \n\n", s, strlen(s));
